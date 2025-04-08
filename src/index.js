d0cb403acc2455e48561227f44cec9423c7fd38d
bitcocom <bitcocom@empas.com>
2025-04-08 10:44:33 +0900

    frontend

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Board from './Board';
+import Cart from './Cart';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Board />
+    <Cart />
   </React.StrictMode>
 );
 
