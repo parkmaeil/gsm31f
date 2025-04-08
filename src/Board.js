96aacc5497dd0deef2d71a65b7dc5dcd6499a3f6
bitcocom <bitcocom@empas.com>
2025-04-08 09:30:39 +0900

    frontend

---
+++ src/Board.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+
+function Board() {
+  const [gsmList, setGsmList] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:8081/api/board')
+      .then(res => res.json())
+      .then(data => setGsmList(data))
+      .catch(err => console.error('에러 발생:', err));
+  }, []);
+
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>📄 GSM 게시글 리스트</h1>
+      {gsmList.length === 0 ? (
+        <p>불러오는 중...</p>
+      ) : (
+        <ul>
+          {gsmList.map(post => (
+            <li key={post.id}>
+              <h3>{post.title}</h3>
+              <p>{post.content}</p>
+              <p>{post.writer}</p>
+              <hr />
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+export default Board;
